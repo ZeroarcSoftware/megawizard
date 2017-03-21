644f8f3beaf67a97235477587a1613fe557acbd8
Justin Slattery <justin@zeroarc.com>
2017-03-21 08:13:57 -0700

    Add prev/next step hooks

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,27 +1,79 @@
-/* MegaWizard - Copyright 2015 Zeroarc Software, LLC
- *
- * Top level container component
- */
-
+// @flow
+// MegaWizard - Copyright 2017 Zeroarc Software, LLC
 'use strict';
 
-// External
 import React from 'react';
 import ReactShallowCompare from 'react-addons-shallow-compare';
-import Immutable from 'immutable';
+import * as Immutable from 'immutable';
 import ClassNames from 'classnames';
 import Autobind from 'autobind-decorator';
 
 // Local
 import Buttons from './Buttons';
 
+type Props = {
+  // Required
+  // steps: Wizard steps config object
+  steps: Immutable.List<*>,
+  // Optional override for complete button text
+  completeButtonText: string,
+  // Optional override for next button text
+  nextButtonText: string,
+  // Optional override for previous button text
+  prevButtonText: string,
+
+  // Optional
+  // onComplete: called when the final wizard complete button is clicked
+  onComplete?: (Immutable.Map<string,*>) => void,
+
+  // called when any step changes, passes step
+  onStepChanged?: (Immutable.Map<string,*>) => void,
+
+  // called when any step changes, passes step
+  onNextStepChanged?: (Immutable.Map<string,*>) => void,
+
+  // called when any step changes, passes step
+  onPreviousStepChanged?: (Immutable.Map<string,*>) => void,
+
+  // called before any step changes, passes step
+  onStepWillChange?: (Immutable.Map<string,*>) => void,
+
+  // called before next step changes, passes step
+  onStepWillChangeToNext?: (Immutable.Map<string,*>) => void,
+
+  // called before previous step changes, passes step
+  onStepWillChangeToPrevious?: (Immutable.Map<string,*>) => void,
+
+  // Optional override for previous button classes
+  prevButtonClasses?: () => void,
+
+  // Optional override for next button classes
+  nextButtonClasses?: string,
+
+  // Optional override for complete button classes
+  completeButtonClasses?: string,
+};
+
+type State = {
+  currentStep: Immutable.Map<string,*>,
+  steps: Immutable.List<*>,
+};
 
 @Autobind
 export default class MegaWizardContainer extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+  props: Props;
+  state: State;
 
-    let visibleSteps = props.steps.filter(s => typeof s.get('visible') === 'undefined' || s.get('visible'));
+  static defaultProps: {
+    completeButtonText: string,
+    nextButtonText: string,
+    prevButtonText: string,
+  };
+
+  constructor(props: Props) {
+    super(props);
+
+    const visibleSteps = props.steps.filter(s => typeof s.get('visible') === 'undefined' || s.get('visible'));
 
     this.state = {
       currentStep: visibleSteps.get(0),
@@ -31,11 +83,11 @@ export default class MegaWizardContainer extends React.Component {
 
   // Because we have steps in state, we need watch for updates
   // and re-filter
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps(nextProps: Props) {
     if (nextProps.steps !== this.props.steps) {
-      let currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
+      const currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
 
-      let visibleSteps = nextProps.steps.filter(s => typeof s.get('visible') === 'undefined' || s.get('visible'));
+      const visibleSteps = nextProps.steps.filter(s => typeof s.get('visible') === 'undefined' || s.get('visible'));
 
       this.setState({
         currentStep: visibleSteps.get(currentStepIndex),
@@ -56,18 +108,38 @@ export default class MegaWizardContainer extends React.Component {
   }
 
   // Check for pending step changes and fire optional callbacks
-  componentWillUpdate(nextProps, nextState) {
+  componentWillUpdate(nextProps: Props, nextState: State) {
     if (nextState.currentStep.get('name') !== this.state.currentStep.get('name')) {
       // Fire global step change callback if it exists
       this.props.onStepWillChange && this.props.onStepWillChange(this.state.currentStep);
+
+      const currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
+      const nextStepIndex = nextState.steps.findIndex(s => s === nextState.currentStep);
+
+      // We are going forward, fire next
+      if (currentStepIndex < nextStepIndex)
+        this.props.onStepWillChangeToNext && this.props.onStepWillChangeToNext(this.state.currentStep);
+      // We are going back, fire previous
+      else
+        this.props.onStepWillChangeToPrevious && this.props.onStepWillChangeToPrevious(this.state.currentStep);
     }
   }
 
   // Check for step changes and fire optional callbacks
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps: Props, prevState: State) {
     if (prevState.currentStep.get('name') !== this.state.currentStep.get('name')) {
       // Fire global step change callback if it exists
       this.props.onStepChanged && this.props.onStepChanged(this.state.currentStep);
+
+      const currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
+      const prevStepIndex = prevState.steps.findIndex(s => s === prevState.currentStep);
+
+      // We went forward, fire next
+      if (prevStepIndex > currentStepIndex)
+        this.props.onNextStepChanged && this.props.onNextStepChanged(this.state.currentStep);
+      // We went back, fire previous
+      else
+        this.props.onPreviousStepChanged && this.props.onPreviousStepChanged(this.state.currentStep);
 
       // Fire entry/exit steps if they exist for previous and current states
       prevState.currentStep.get('onExit') && prevState.currentStep.get('onExit')();
@@ -75,29 +147,29 @@ export default class MegaWizardContainer extends React.Component {
     }
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextProps: Props, nextState: State) {
     return ReactShallowCompare(this, nextProps, nextState);
   }
 
   render() {
-    let currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
+    const currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
 
     // If defined, execute validators and check the result to determine if the step can advancex;
-    let nextStepAllowed = typeof this.state.currentStep.get('nextValidator') === 'undefined' || !!this.state.currentStep.get('nextValidator');
+    const nextStepAllowed = typeof this.state.currentStep.get('nextValidator') === 'undefined' || !!this.state.currentStep.get('nextValidator');
     // In addition to the prevValidator, make sure this isn't the first step
-    let prevStepAllowed = currentStepIndex !== 0 &&
+    const prevStepAllowed = currentStepIndex !== 0 &&
       (typeof this.state.currentStep.get('prevValidator') === 'undefined' || !!this.state.currentStep.get('prevValidator'));
 
     // Build out summary column of steps
-    let stepNames = this.state.steps.map((step,index) => {
+    const stepNames = this.state.steps.map((step,index) => {
       // Current step is highlighted in bold
-      let nameClasses = ClassNames('list-group-item', {
+      const nameClasses = ClassNames('list-group-item', {
         'text-bold': index === currentStepIndex
       });
 
       // Current step has success label, previous steps are primary
       // future steps are default
-      let numberClasses = ClassNames('label', {
+      const numberClasses = ClassNames('label', {
         'label-primary': index < currentStepIndex,
         'label-success': index === currentStepIndex
       });
@@ -149,53 +221,29 @@ export default class MegaWizardContainer extends React.Component {
   // Custom methods
   //
 
-  handlePreviousStepClick(e) {
+  handlePreviousStepClick(e: SyntheticInputEvent) {
     e.stopPropagation();
-    let currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
+    const currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
 
     if (currentStepIndex > 0) {
       this.setState({currentStep: this.state.steps.get(currentStepIndex - 1)});
     }
   }
 
-  handleNextStepClick(e) {
+  handleNextStepClick(e: SyntheticInputEvent) {
     e.stopPropagation();
-    let currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
+    const currentStepIndex = this.state.steps.findIndex(s => s === this.state.currentStep);
 
     if (currentStepIndex < this.state.steps.count()) {
       this.setState({currentStep: this.state.steps.get(currentStepIndex + 1)});
     }
   }
 
-  handleCompleteStepClick(e) {
+  handleCompleteStepClick(e: SyntheticInputEvent) {
     e.stopPropagation();
-    this.props.onComplete(this.state.currentStep);
+    this.props.onComplete && this.props.onComplete(this.state.currentStep);
   }
 }
-
-MegaWizardContainer.propTypes = {
-  // Required
-  // steps: Wizard steps config object
-  steps: React.PropTypes.instanceOf(Immutable.List).isRequired,
- // Optional override for complete button text
-  completeButtonText: React.PropTypes.string.isRequired,
-  // Optional override for next button text
-  nextButtonText: React.PropTypes.string.isRequired,
-  // Optional override for previous button text
-  prevButtonText: React.PropTypes.string.isRequired,
-
-  // Optional
-  // onComplete: called when the final wizard complete button is clicked
-  onComplete: React.PropTypes.func,
-  // called when any step changes, passes step index
-  onStepChanged: React.PropTypes.func,
-  // Optional override for previous button classes
-  prevButtonClasses: React.PropTypes.string,
-  // Optional override for next button classes
-  nextButtonClasses: React.PropTypes.string,
-  // Optional override for complete button classes
-  completeButtonClasses: React.PropTypes.string,
- };
 
 MegaWizardContainer.defaultProps = {
   completeButtonText: 'Done',
